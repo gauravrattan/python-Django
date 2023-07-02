68762882e99bdf84d17f72580d76aab7d0bd3b79
Gaurav Rattan <100664099+gauravrattan@users.noreply.github.com>
2023-07-03 01:22:08 +0530

    Update entrypoint.sh

--- Django-Container/src/config/entrypoint.sh
+++ Django-Container/src/config/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RUN_PORT="8000"
+RUN_PORT="80"
 
 /opt/venv/bin/python manage.py migrate --noinput
 /opt/venv/bin/gunicorn cfehome.wsgi:application --bind "0.0.0.0:${RUN_PORT}" --daemon --access-logfile /dev/stdout --capture-output --log-level info
